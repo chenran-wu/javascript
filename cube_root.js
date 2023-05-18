4a4ee8b94d9590b29c8d4fae97f45fb14a30ef60
Yu <chenran1129@gmail.com>
2023-05-18 17:39:32 +0300

    A bug needs to be fixed.

---
+++ cube_root.js
@@ -0,0 +1,19 @@
+function is_good_enough(guess, x) {
+  return Math.abs(guess * guess * guess - x) / x < 0.001
+}
+
+function improve(guess, x) {
+  return (x / guess * guess + 2 * guess) / 3
+}
+
+function cube_root_iter(guess, x) {
+  return is_good_enough(guess, x)
+    ? guess
+    : cube_root_iter(improve(guess, x), x)
+}
+
+function cube_root(x) {
+  return cube_root_iter(1, x)
+}
+
+console.log(cube_root(27))
