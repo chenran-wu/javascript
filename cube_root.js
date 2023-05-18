10f464e703d2a097155eb8d920721b1f15240cb5
Yu <chenran1129@gmail.com>
2023-05-18 17:45:51 +0300

    Bug fixed.

--- cube_root.js
+++ cube_root.js
@@ -3,7 +3,7 @@ function is_good_enough(guess, x) {
 }
 
 function improve(guess, x) {
-  return (x / guess * guess + 2 * guess) / 3
+  return (x / (guess * guess) + 2 * guess) / 3
 }
 
 function cube_root_iter(guess, x) {
